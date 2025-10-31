0312bf2dd10236f599131b4b5286b89d90cda6f0
huynhhung1998 <hr.vp.huynhhung@gmail.com>
2025-10-31 17:31:31 +0700

    Update Link FE

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://hhfashion.onrender.com",
+      "https://hhfashion-2b62.onrender.com",
     ],
     credentials: true,
   })
